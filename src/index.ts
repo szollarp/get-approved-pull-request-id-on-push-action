e239691703c24bee00a78c3d7398499e1a312da8
Peter Szollar <szollarp@gmail.com>
2020-10-16 22:33:57 +0200

    add: init

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token', { required: true });
+   
+    const {context, getOctokit} = github;
+    const octokit = getOctokit(token);
+
+    const pull_requests = await octokit.issues.listPullRequestsAssociatedWithCommit({
+      ...context.repo,
+      commit_sha: context.sha
+    });
+
+    console.log(pull_requests);
+  }
+  catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
